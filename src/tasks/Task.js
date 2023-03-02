30e4c3fb7cc8387937dc15ca123d6b78599634b9
Aesop <ee.changsub@gmail.com>
2023-03-02 21:55:52 +0900

    task 추가 시 input box 입력

--- src/tasks/Task.js
+++ src/tasks/Task.js
@@ -1,16 +1,33 @@
-function Task() {
-    const addTask = () => {
-        console.log('input 창 생겨야함');
-    }
+import { useState } from 'react';
 
-    return (
-        <div className={Task.name}>
-            <header className="Task-header">
-                <p>Tasks</p>
-                <button onClick={addTask}>+</button>
-            </header>
-        </div>
-    )
+function Task() {
+	const [showInput, setShotInput] = useState(false);
+
+	const addTask = () => {
+		setShotInput(!showInput); // input 다시 숨기기 위해서
+	};
+
+	const handleInputChange = (event) => {};
+
+	const handleInputSubmit = (event) => {
+		event.preventDefault();
+		//todo: 저장 요청
+		setShotInput(false);
+	};
+
+	return (
+		<div className={Task.name}>
+			<header className="Task-header">
+				<p>Tasks</p>
+				<button onClick={addTask}>+</button>
+			</header>
+			{showInput && (
+				<form onSubmit={handleInputSubmit}>
+					<input type="text" onChange={handleInputChange} />
+				</form>
+			)}
+		</div>
+	);
 }
 
 export default Task;
