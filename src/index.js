b2ca7e9ee908f50cd86b44389f7efbc9bb2f0e18
Aesop <ee.changsub@gmail.com>
2023-03-18 23:00:08 +0900

    graphql 쿼리 위한 준비

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4101/graphql',
+  }),
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
