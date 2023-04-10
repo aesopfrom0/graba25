f9c064a6bf851565ffc8a44853af3be14cfaf8d4
이창섭 <aesop@coinone.com>
2023-04-10 18:55:47 +0900

    clock-header 및 main css 수정

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import Task from './tasks/Task';
 import Clock from './clock/Clock';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Gimbap Focus</p>
-				<Clock />
-				<Task />
-				<a
-					className="App-link"
-					href="https://aesop.oopy.io"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					개발자 블로그
-				</a>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <p>GRABA 25</p>
+        <Clock />
+        <Task />
+        <a
+          className="App-link"
+          href="https://aesop.oopy.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          개발자 블로그
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/clock/Clock.js
+++ src/clock/Clock.js
@@ -1,47 +1,67 @@
 import { useEffect, useState } from 'react';
 
 function Clock() {
-	const initialGivenSeconds = 5;
-	const [isStarted, setIsStarted] = useState(false);
-	const [timeRemaining, setTimeRemaining] = useState(initialGivenSeconds);
+  const initialGivenSeconds = 5;
+  const [isStarted, setIsStarted] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(initialGivenSeconds);
+  const [activeTab, setActiveTab] = useState('pomodoro');
 
-	useEffect(() => {
-		let timer;
-		if (isStarted && timeRemaining > 0) {
-			timer = setInterval(() => {
-				setTimeRemaining((time) => time - 1);
-			}, 1000);
-		}
+  useEffect(() => {
+    let timer;
+    if (isStarted && timeRemaining > 0) {
+      timer = setInterval(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    }
 
-		if (timeRemaining === 0) {
-			setTimeRemaining(initialGivenSeconds);
-			setIsStarted(false);
-		}
+    if (timeRemaining === 0) {
+      setTimeRemaining(initialGivenSeconds);
+      setIsStarted(false);
+    }
 
-		return () => clearInterval(timer);
-	}, [isStarted, timeRemaining]);
+    return () => clearInterval(timer);
+  }, [isStarted, timeRemaining]);
 
-	const formatTime = (timeInSeconds) => {
-		const minutes = Math.floor(timeInSeconds / 60);
-		const seconds = timeInSeconds % 60;
-		return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-	};
+  const formatTime = (timeInSeconds) => {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
 
-	const handleStart = (event) => {
-		setIsStarted(true);
-	};
+  const handleStart = (event) => {
+    setIsStarted(true);
+  };
 
-	return (
-		<div className={Clock.name}>
-			<header className="Clock-header">
-				<a>Pomodoro </a>
-				<a>Short Break </a>
-				<a>Long Break</a>
-			</header>
-			<p className="Clock-main">{formatTime(timeRemaining)}</p>
-			<button onClick={handleStart}>Start</button>
-		</div>
-	);
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className={Clock.name}>
+      <header className="Clock-header">
+        <a
+          className={activeTab === 'pomodoro' ? 'active' : ''}
+          onClick={() => handleTabClick('pomodoro')}
+        >
+          Pomodoro{' '}
+        </a>
+        <a
+          className={activeTab === 'shortBreak' ? 'active' : ''}
+          onClick={() => handleTabClick('shortBreak')}
+        >
+          Short Break{' '}
+        </a>
+        <a
+          className={activeTab === 'longBreak' ? 'active' : ''}
+          onClick={() => handleTabClick('longBreak')}
+        >
+          Long Break
+        </a>
+      </header>
+      <p className="Clock-main">{formatTime(timeRemaining)}</p>
+      <button onClick={handleStart}>Start</button>
+    </div>
+  );
 }
 
 export default Clock;
