dbf172c82a610a888369263ba8620c1b15d5bcd4
Aesop <ee.changsub@gmail.com>
2023-04-18 22:29:33 +0900

    최초 gql 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.png';
 import './App.css';
-import Task from './tasks/Task';
 import Clock from './clock/Clock';
+import Task from './tasks/Task';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>GRABA 25</p>
         <Clock />
         <Task />
         <a
-          className="App-link"
-          href="https://aesop.oopy.io"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://aesop.oopy.io'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           개발자 블로그
         </a>
--- src/tasks/Task.js
+++ src/tasks/Task.js
@@ -1,33 +1,48 @@
 import { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
+
+const CREATE_TASK_QUERY = gql`
+    mutation CreateTask($task: CreateTaskInput!) {
+        createTask(task: $task) {
+            title
+        }
+    }
+`;
 
 function Task() {
-	const [showInput, setShotInput] = useState(false);
+  const [showInput, setShotInput] = useState(false);
+  const [createTask] = useMutation(CREATE_TASK_QUERY);
+  const addTask = () => {
+    setShotInput(!showInput); // input 다시 숨기기 위해서
+  };
+  const handleInputSubmit = async (event) => {
+    event.preventDefault();
 
-	const addTask = () => {
-		setShotInput(!showInput); // input 다시 숨기기 위해서
-	};
+    const formData = new FormData(event.target);
+    const taskInput = {
+      title: formData.get('title'),
+      userId: 1,
+    };
 
-	const handleInputChange = (event) => {};
+    try {
+      const { data } = await createTask({ variables: { task: taskInput } });
+      console.log('Task created:', data);
+    } catch (error) {
+      console.error('Failed to create task:', error);
+    }
+  };
 
-	const handleInputSubmit = (event) => {
-		event.preventDefault();
-		//todo: 저장 요청
-		setShotInput(false);
-	};
-
-	return (
-		<div className={Task.name}>
-			<header className="Task-header">
-				<p>Tasks</p>
-				<button onClick={addTask}>+</button>
-			</header>
-			{showInput && (
-				<form onSubmit={handleInputSubmit}>
-					<input type="text" onChange={handleInputChange} />
-				</form>
-			)}
-		</div>
-	);
+  return (
+    <div className={Task.name}>
+      <header className='Task-header'>
+        <p>Tasks</p>
+        <button onClick={addTask}>+</button>
+      </header>
+      {showInput && (<form onSubmit={handleInputSubmit}>
+        <input type='text' name='title' />
+      </form>)}
+    </div>
+  );
 }
 
 export default Task;
